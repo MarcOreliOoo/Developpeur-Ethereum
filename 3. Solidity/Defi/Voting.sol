2388008d6338decee8f2590f6ba232f761e8049d
MarcOreliOoo <94016378+MarcOreliOoo@users.noreply.github.com>
2021-12-12 15:59:09 +0100

    Update Voting.sol

--- 3. Solidity/Defi/Voting.sol
+++ 3. Solidity/Defi/Voting.sol
@@ -44,19 +44,15 @@ contract Voting is Ownable{
     }
 
     mapping (address => Voter) private comptesWL;
-    //mapping (uint => Proposal) private listProposal;
     Proposal[] public proposals;
-    //uint public proposalId = 0;
+    
     uint public winningProposalId;
     uint public nbVote = 0;
     uint public totalVoter = 0;    
 
     
     modifier atStage(WorkflowStatus _status) {
-        require(
-            wfStatus == _status,
-            "Function cannot be called at this time."
-        );
+        require(wfStatus == _status, "Function cannot be called at this time.");
         _;
     }
 
@@ -97,11 +93,12 @@ contract Voting is Ownable{
     function registeringWL(address[] memory arrayWL) public onlyOwner atStage(WorkflowStatus.RegisteringVoters){
         Voter memory vote;
         for(uint i=0;i<arrayWL.length;i++){
-            vote = Voter(true,false,0);
-            //Si l'adresse est déjà existante on écrase
-            comptesWL[arrayWL[i]] = vote;
-            totalVoter++;
-            emit VoterRegistered(arrayWL[i]);
+            if(!comptesWL[arrayWL[i]].isRegistered){
+                vote = Voter(true,false,0);
+                comptesWL[arrayWL[i]] = vote;
+                totalVoter++;
+                emit VoterRegistered(arrayWL[i]);
+            }
         }
     }
 
@@ -123,10 +120,6 @@ contract Voting is Ownable{
     */
     function registeringProposal(string memory _description) public atStage(WorkflowStatus.ProposalsRegistrationStarted) senderRegistred{
         proposals.push(Proposal(_description,0));
-        //listProposal[proposalId] = Proposal(_description,0);
-        //proposalId++;
-        //TODO Que se passe-t-il si plusieurs propositions sont les mêmes ?
-        //emit ProposalRegistered(proposalId);
         emit ProposalRegistered(proposals.length);
     }
 
@@ -137,7 +130,6 @@ contract Voting is Ownable{
     */
     function endingProposalSession() public onlyOwner atStage(WorkflowStatus.ProposalsRegistrationStarted){
         //Proposal session over but... do we have at least one vote ?
-        //require(proposalId >= 1,"Not enough proposition to start a vote");
         require(proposals.length >= 1,"Not enough proposition to start a vote");
         nextStage();
     }
@@ -160,7 +152,6 @@ contract Voting is Ownable{
         //N'a pas déjà voté
         require(!comptesWL[msg.sender].hasVoted,"You already voted");        
         //Vote pour une propal existante
-        //require(proposalId >= _proposalId,"Proposal wished doesnt exist");
         require(proposals.length >= _proposalId,"Proposal wished doesnt exist");
 
         //Mise à jour du "à voter"
@@ -168,7 +159,6 @@ contract Voting is Ownable{
         comptesWL[msg.sender].votedProposalId = _proposalId;
 
         //Mise à jour du count de vote pour la proposition votée
-        //listProposal[_proposalId].voteCount++;
         proposals[_proposalId].voteCount++;
         //Le nb de vote total augmente
         nbVote++;
@@ -196,12 +186,6 @@ contract Voting is Ownable{
     */
     function countVote() public onlyOwner atStage(WorkflowStatus.VotingSessionEnded){
         uint maxCount=0;
-        /*for(uint i = 0;i<proposalId;i++){
-            if(listProposal[i].voteCount > maxCount){
-                maxCount = listProposal[i].voteCount;
-                winningProposalId = i;
-            }
-        }*/
         for(uint i = 0;i<proposals.length;i++){
             if(proposals[i].voteCount > maxCount){
                 maxCount = proposals[i].voteCount;
@@ -227,7 +211,7 @@ contract Voting is Ownable{
     function reInitStatus() public onlyOwner atStage(WorkflowStatus.VotesTallied) {
         nextStage();
         
-        //Re init la liste des proposals, la liste des WL. Penser à changer le mapping(uint => Proposal) en tableau de proposal directement. Pas d'utilité d'un tel mapping)
+        //Re init la liste des proposals, la liste des WL, la winningProposal, le totalVoter etc.)
     }
 
 }
